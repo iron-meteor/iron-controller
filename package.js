253fde6d4a93bf3132647bbb4f745ffe6f4796ce
Chris Mather <mather.chris@gmail.com>
2014-07-25 16:21:39 -0700

    update package.js for old package system

--- package.js
+++ package.js
@@ -20,25 +20,25 @@ Package.on_use(function (api) {
   // reactive state variables
   api.use('reactive-dict');
 
-  api.use('iron:core');
-  api.imply('iron:core');
+  api.use('iron-core');
+  api.imply('iron-core');
 
-  api.use('iron:layout');
-  api.use('iron:dynamic-template');
+  api.use('iron-layout');
+  api.use('iron-dynamic-template');
 
-  api.addFiles('lib/wait_list.js', 'client');
-  api.addFiles('lib/controller.js', ['client', 'server']);
+  api.add_files('lib/wait_list.js', 'client');
+  api.add_files('lib/controller.js', ['client', 'server']);
 });
 
 Package.on_test(function (api) {
-  api.use('iron:controller');
-  api.use('iron:layout');
+  api.use('iron-controller');
+  api.use('iron-layout');
   api.use('tinytest');
   api.use('test-helpers');
   api.use('deps');
   api.use('templating');
 
-  api.addFiles('test/controller_test.html', 'client');
-  api.addFiles('test/wait_list_test.js', 'client');
-  api.addFiles('test/controller_test.js', 'client');
+  api.add_files('test/controller_test.html', 'client');
+  api.add_files('test/wait_list_test.js', 'client');
+  api.add_files('test/controller_test.js', 'client');
 });
