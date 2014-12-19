f7c87d54f368fda45f9796e0d9ef109b42e99c26
Chris Mather <mather.chris@gmail.com>
2014-12-18 19:10:47 -0800

    bump version to 1.0.6

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: 'Controller class for dynamic layouts.',
-  version: '1.0.4',
+  version: '1.0.6',
   git: 'https://github.com/eventedmind/iron-controller.git'
 });
 
@@ -17,11 +17,11 @@ Package.on_use(function (api) {
 
   api.use('templating');
 
-  api.use('iron:core@1.0.4');
+  api.use('iron:core@1.0.6');
   api.imply('iron:core');
 
-  api.use('iron:layout@1.0.4');
-  api.use('iron:dynamic-template@1.0.4');
+  api.use('iron:layout@1.0.6');
+  api.use('iron:dynamic-template@1.0.6');
 
   api.add_files('lib/wait_list.js', 'client');
   api.add_files('lib/controller.js');
